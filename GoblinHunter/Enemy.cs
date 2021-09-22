068d76bb679d578abf6e7b932f5eb0bb11d0d13d
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2021-09-22 21:29:34 +0200

    penis

--- GoblinHunter/Enemy.cs
+++ GoblinHunter/Enemy.cs
@@ -8,9 +8,26 @@ namespace GoblinHunter
     {
         public Random rnd = new Random();
         public String EnemyClassName = " ";
-        protected Enemy(int _X, int _Y, TileType _TOT) : base(_X, _Y, _TOT)
-        {
+        private int damage;
 
+        public int DAMAGE
+        {
+            get { return damage; }
+            set { damage = value; }
+        }
+
+        private int maxHp;
+
+        public int MAXHP
+        {
+            get { return maxHp; }
+            set { maxHp = value; }
+        }
+
+        protected Enemy(int _X, int _Y, TileType _TOT,int _Damage, int _MAXHP) : base(_X, _Y, _TOT)
+        {
+            MaxHp = _MAXHP;
+            DAMAGE = _Damage;
         }
 
         public override string ToString()
