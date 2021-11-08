b18f4a2bb180e332ae6938e7bc4238b2f963cfed
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2021-11-08 14:35:08 +0200

    continued fixing the code for task 1

--- GoblinHunter/GameEngine.cs
+++ GoblinHunter/GameEngine.cs
@@ -17,10 +17,10 @@ namespace GoblinHunter
         //this method moves the player 
         public bool movePlayer(Movement movement)
         {
-            return
+            return;
         }
         //declaring the constructor and pulling the variables that are neccessary in this class
-        public GameEngine(int _X, int _Y, TileType _TOT, int _Damage, int _MAXHP, int _mapMinHGHT, int _mapMaxHGHT, int _mapMinWDTH, int _mapMaxWDTH) : base(_X, _Y, _TOT, _Damage, _MAXHP, _mapMinHGHT, _mapMaxHGHT, _mapMinWDTH, _mapMaxWDTH)
+        public GameEngine(int _X, int _Y, TileType _TOT, int _Damage, int _MAXHP, int _mapMinHGHT, int _mapMaxHGHT, int _mapMinWDTH, int _mapMaxWDTH) : base(_X, _Y," ", _TOT, _mapMinHGHT, _mapMaxHGHT, _mapMinWDTH, _mapMaxWDTH,0)
         {
             game = new Map();
 
